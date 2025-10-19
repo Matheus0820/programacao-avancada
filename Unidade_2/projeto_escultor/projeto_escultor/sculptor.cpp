d5d6aa0b2aeda73b6b57259f309b3c4949eec8d6
Matheus0820 <matheus.ramos.703@ufrn.edu.br.com>
2025-10-19 19:08:48 -0300

    feat: Adicionando os metodos de plotar e e cortar a caixa e a esfera

--- Unidade_2/projeto_escultor/projeto_escultor/sculptor.cpp
+++ Unidade_2/projeto_escultor/projeto_escultor/sculptor.cpp
@@ -27,8 +27,99 @@ void Sculptor::cutVoxel(int x, int y, int z) {
 
 void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1) {
 // Criar voxels no intervelo de x0 - x1, y0 - y1, z0 - z1 e definir a cor como a atual, o show como true
+
+    // Definindo intervalo padrão no modo crescente
+    int aux; // Variável auxíliar
+    if(x0 > x1) {
+        aux = x0;
+        x0 = x1;
+        x1 = aux;
+    }
+    if (y0 > y1) {
+        aux = y0;
+        y0 = y1;
+        y1 = aux;
+    }
+    if (z0 > z1) {
+        aux = z0;
+        z0 = z1;
+        z1 = aux;
+    }
+
+    // Fazendo com que todo voxel dentro dos intervalos dados sejam visivéis e com a cor atual do desenho;
+    for (int i = x0; i <= x1; i++) {
+        for (int j = y0; j <= y1; j++) {
+            for(int w = z0; w = z1; w++) {
+                v[i][j][w].show = true;
+                v[i][j][w].r = r;
+                v[i][j][w].g = g;
+                v[i][j][w].b = b;
+                v[i][j][w].a = a;
+            }
+        }
+    }
 }
 
 void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1) {
     // Fazer o show dos voxels no intervalo x0 - x1, y0 - y1, z0 - z1 igual a false
+
+    // Definindo intervalo padrão no modo crescente
+    int aux; // Variável auxíliar
+    if(x0 > x1) {
+        aux = x0;
+        x0 = x1;
+        x1 = aux;
+    }
+    if (y0 > y1) {
+        aux = y0;
+        y0 = y1;
+        y1 = aux;
+    }
+    if (z0 > z1) {
+        aux = z0;
+        z0 = z1;
+        z1 = aux;
+    }
+
+    // Fazendo com que todo voxel dentro dos intervalos dados tenham a visibilidade (show) igual a false;
+    for (int i = x0; i <= x1; i++) {
+        for (int j = y0; j <= y1; j++) {
+            for(int w = z0; w = z1; w++) {
+                v[i][j][w].show = false;
+            }
+        }
+    }
+
+}
+
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius) {
+    // Vê uma formar de vê os pontos que estão contidos pela esfera,
+    // ou seja satisfazem a equação da esfera -> (xcenter - a)**2 + (ycenter - b)**2 + (zcenter - c)**2 <= radius**2
+    for (int i = 0; i <= nx; i++) {
+        for (int j = 0; j <= ny; j++) {
+            for(int w = 0; w = nz; w++) {
+                if ((xcenter - i)**2 + (ycenter - j)**2 + (zcenter - w)**2 <= radius**2) {
+                    v[i][j][w].show = true;
+                    v[i][j][w].r = r;
+                    v[i][j][w].g = g;
+                    v[i][j][w].b = b;
+                    v[i][j][w].a = a;
+                }
+            }
+        }
+    }
+}
+
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius) {
+    // Vê uma formar de vê os pontos que estão contidos pela esfera,
+    // ou seja satisfazem a equação da esfera -> (xcenter - a)**2 + (ycenter - b)**2 + (zcenter - c)**2 <= radius**2
+    for (int i = 0; i <= nx; i++) {
+        for (int j = 0; j <= ny; j++) {
+            for(int w = 0; w = nz; w++) {
+                if ((xcenter - i)**2 + (ycenter - j)**2 + (zcenter - w)**2 <= radius**2) {
+                    v[i][j][w].show = false;
+                }
+            }
+        }
+    }
 }
