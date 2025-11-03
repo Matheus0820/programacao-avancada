0a39281fd5d39ce30ea80f448b2184ff603be524
Matheus0820 <mr7052954@gmail.com>
2025-11-03 07:59:44 -0300

    feat: Aumentando o tamanho do modelo 3d

--- Unidade_2/projeto_escultor/projeto_escultor/main.cpp
+++ Unidade_2/projeto_escultor/projeto_escultor/main.cpp
@@ -7,34 +7,37 @@
 using namespace std;
 
 int main() {
+    int tam = 300; // Tamanho da área de pintura
+
     // Gerando semente do random com base no tempo
     std::srand(std::time(nullptr));
 
     // Criando objeto da classe sculptor
-    Sculptor obj1(100, 100, 100);
+    Sculptor obj1(300, 300, 300);
 
     // Gerando asteroides aleatórios
     obj1.setColor(1.0, 1.0, 1.0, 0.5);
-    for (int i = 0; i < 300; ++i) {
-        int x = std::rand() % 100;
-        int y = std::rand() % 100;
-        int z = std::rand() % 100;
+    for (int i = 0; i < tam * 10; ++i) {
+        int x = std::rand() % tam;
+        int y = std::rand() % tam;
+        int z = std::rand() % tam;
         obj1.putVoxel(x, y, z);
     }
+
 
     // Fazendo um buraco negro
 
     // Disco de acreção
     obj1.setColor(1.0, 1.0, 0, 0.5);
-    obj1.putEllipsoid(50, 50, 50, 30, 3, 30);
-    obj1.cutEllipsoid(50, 50, 50, 20, 3, 20);
+    obj1.putEllipsoid(tam/2, tam/2, tam/2, 60, 3, 60);
+    obj1.cutEllipsoid(tam/2, tam/2, tam/2, 45, 3, 45);
 
-    obj1.putEllipsoid(50, 50, 50, 30, 30, 3);
-    obj1.cutEllipsoid(50, 50, 50, 20, 20, 3);
+    obj1.putEllipsoid(tam/2, tam/2, tam/2, 60, 60, 3);
+    obj1.cutEllipsoid(tam/2, tam/2, tam/2, 45, 45, 3);
 
     // Esfera negra
     obj1.setColor(0, 0, 0, 1);
-    obj1.putSphere(50, 50, 50, 10);
+    obj1.putSphere(tam/2, tam/2, tam/2, 25);
 
     // Planeta miler
     obj1.setColor(0.24, 0.32, 0.56, 1);
@@ -42,10 +45,7 @@ int main() {
 
     // Endurance
     obj1.setColor(0.5, 0.5, 0.5, 0.5);
-    obj1.putEllipsoid(85, 85, 85, 2, 2, 1);
-
-    obj1.setColor(0.7, 0.7, 0.7, 0.7);
-    obj1.putBox(84, 86, 84, 86, 83, 87);
+    obj1.putEllipsoid(90, 90, 90, 3, 3, 1);
 
 
 
