7803b27e96b51953335a9f8ce06e0afc725c2198
Matheus0820 <mr7052954@gmail.com>
2025-12-01 11:46:34 -0300

    feat: Adicionando plotagem do seno

--- Unidade_3/projetos_aula/seno_plot_qt/plotter.cpp
+++ Unidade_3/projetos_aula/seno_plot_qt/plotter.cpp
@@ -35,6 +35,35 @@ void Plotter::paintEvent(QPaintEvent *event) {
     // Desenhando um retangulo que sobre a tela inteira
     painter.drawRect(0, 0, width(), height());
 
+    // Pintando uma linha no meio da tela
+    painter.drawLine(0, height()/2, width(), height()/2);
 
+    /* Plotando o Seno */
+    // Mudando a cor da caneta para preto
+    pen.setColor(QColor(0, 0, 0));
+    pen.setWidth(2);
+    painter.setPen(pen);
+
+
+    // Variáveis auxíliares:
+    int x0, y0, x1, y1;
+    x0 = 0; // Lado esquedo da tela
+    y0 = height()/2; // Metade da tela
+
+    float porc_amplitude = 1;
+    float fase = 0.0;
+    float frequencia = 1; // Frequência em Hz
+
+    // Laço que vai atualizar o valor de y1 em função do seno
+    for(x1 = 1; x1 < width(); x1++) {
+        y1 = height()/2 - (height()/2) * porc_amplitude * sin(fase + (frequencia * 2 * PI * x1)/width());
+
+        // Desenhando o ponto (x1, y1)
+        painter.drawLine(x0, y0, x1, y1);
+
+        // Atualizando os valores de x0 e y0 para o velores atuais de x1 e y1
+        x0 = x1;
+        y0 = y1;
+    }
 
 }
